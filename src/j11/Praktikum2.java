6b994810dbf1698cb8ac78be77b359922c0333de
Lantip Mukti Mumpuni <Lantip Mukti Mumpuni@DESKTOP-1RKVMV2>
2018-08-14 13:06:49 +0700

    Tambah Konversi.

--- src/j11/Praktikum2.java
+++ src/j11/Praktikum2.java
@@ -12,6 +12,7 @@ public class Praktikum2 {
         String identitas = " Lantip Mukti Mumpuni / XR3 / 18";
         tampilJudul(identitas);
         String kalimat = tampilInput();
+        String convert = vocal2Angka(kalimat);
     }
     private static String tampilInput()
     {
@@ -22,5 +23,15 @@ public class Praktikum2 {
         System.out.print("Kalimat Asli : " + kalimat);
         
         return kalimat;
+    }
+    private static String vocal2Angka(String kalimat)
+    {
+     char [][] arConvert = {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
+     
+     kalimat = kalimat.toLowerCase();
+     for(int i = 0; i<arConvert.length; i++)
+         kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
+     
+     return kalimat;
     }
 }
