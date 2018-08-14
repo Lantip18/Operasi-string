2d98b835e8b8b918df838dc5d0f06ab82d17629d
Lantip Mukti Mumpuni <Lantip Mukti Mumpuni@DESKTOP-1RKVMV2>
2018-08-14 13:04:09 +0700

    Initial Commit.

--- src/j11/Praktikum2.java
+++ src/j11/Praktikum2.java
@@ -11,41 +11,5 @@ public class Praktikum2 {
     {
         String identitas = " Lantip Mukti Mumpuni / XR3 / 18";
         tampilJudul(identitas);
-        String kalimat = tampilInput();
-        String convert = vocal2Angka(kalimat);
-        tampilPerkata(kalimat, convert);
-        tampilHasil(convert);
     }
-    private static String tampilInput()
-    {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Masukkan kalimat : ");
-        String kalimat = sc.nextLine();
-        System.out.println("Kalimat Asli : " + kalimat);
-        
-        return kalimat;
-    }
-    private static String vocal2Angka(String kalimat)
-    {
-     char [][] arConvert = {{'a','4'},{'i','1'},{'u','2'},{'e','3'},{'o','0'}};
-     
-     kalimat = kalimat.toLowerCase();
-     for(int i = 0; i<arConvert.length; i++)
-         kalimat = kalimat.replace(arConvert[i][0], arConvert[i][1]);
-     
-     return kalimat;
-    }
-    private static void tampilPerkata(String kalimat, String convert)
-    {
-        String[] arrKal = kalimat.split(" ");
-        String[] arrCon = kalimat.split(" ");
-        
-        for(int i =0 ; i<arrKal.length ; i++)
-            System.out.println(arrKal[i]+" => "+arrCon[i]);
-    }
-    private static void tampilHasil(String convert)
-    {
-        System.out.println("Kalimat Alay Angka : " + convert);
-    }
-    }
+}
