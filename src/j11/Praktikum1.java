dcce1525f750ad774488a18a73320d31c613169d
Lantip Mukti Mumpuni <Lantip Mukti Mumpuni@DESKTOP-1RKVMV2>
2018-08-14 12:58:04 +0700

    Coba method format.

--- src/j11/Praktikum1.java
+++ src/j11/Praktikum1.java
@@ -65,6 +65,12 @@ public class Praktikum1 {
         System.out.println("pecah berdasarkan huruf 'r' di variable tolean : ");
         for(int i=0;i<arrTr.length;i++)
             System.out.println(arrTr[i]);
+        
+        double d = 1545454000;
+        System.out.println(String.format("Uang saya %,.4f", d));
+        
+        System.out.println(String.format(Locale.forLanguageTag("in-ID"),"Uang saya Rp %,.2f", d));
+        
     }
     
 }
