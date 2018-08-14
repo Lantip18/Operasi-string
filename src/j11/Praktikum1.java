e17b028a383a0823138a4055f190cf972be12604
Lantip Mukti Mumpuni <Lantip Mukti Mumpuni@DESKTOP-1RKVMV2>
2018-08-14 12:50:10 +0700

    Coba method length & isEmpty

--- src/j11/Praktikum1.java
+++ src/j11/Praktikum1.java
@@ -7,6 +7,15 @@ public class Praktikum1 {
     public static void main(String[] args) {
         String identitas = "Lantip Mukti M. / X RPL 3 / 18";
         System.out.println("identitas : " + identitas);
+        
+        String x = "Operasi";
+        System.out.println("isi variable x : "+ x);
+        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variable y : " + y);
+        System.out.println("y adalah kosong : "+ y.isEmpty());
     }
     
 }
